bf23050eefd04e17f388499f0fddf48374603746
Jan Rathmer <jan.rathmer@gmail.com>
2021-10-06 22:52:22 +0200

    Renaming

--- touchInput.ino
+++ touchInput.ino
@@ -27,67 +27,68 @@ void initTouch(void)
         Serial.println(TOUCH_I2C_ADD, HEX);
     }
 
-
-  // Init Switch-Button. Rotary is initialized with "myEncoder"
-//   pinMode(BUTTON_PIN, INPUT);
-//   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  previousButtonState = false;
-
   // Init Time Measurement
+  previousButtonState = false;
   startTimeButtonPressed = millis();
   endTimeButtonPressed = millis();
 }
 
 void loopTouch(void)
 {
-//   int buttonPressed = digitalRead(BUTTON_PIN);
+    loopOffsetButton();
+}
 
-  boolean buttonPressed = isButtonPressed();
+void loopOffsetButton()
+{
+  boolean setOffsetPressed = isSetOffsetPressed();
 
-  if(previousButtonState == false && buttonPressed == true) {
+  if(previousButtonState == false && setOffsetPressed == true) {
     previousButtonState = true;
-    startMeasure();
+    startMeasureOffsetButton();
   }
 
-  if(previousButtonState == true && buttonPressed == true) {
+  if(previousButtonState == true && setOffsetPressed == true) {
     // TODO Zeit zählen!!
-    Serial.print("....");
+    Serial.print(".");
   }
 
-  if(previousButtonState == true && buttonPressed == false) {
+  if(previousButtonState == true && setOffsetPressed == false) {
     previousButtonState = false;
-    endMeasure();
+    endMeasureOffsetButton();
   }
 
-  if(buttonPressed == true) {
+  if(setOffsetPressed == true) {
     previousButtonState = true;
   } else {
     previousButtonState = false;
-  }
+  }    
 }
 
-boolean isButtonPressed(void)
+boolean isSetOffsetPressed(void)
 {
 // Read Touch Values
     int pos[2] = {0, 0};
     ft6236_pos(pos);
     
-    // Serial.printf("%d,%d\n", pos[0], pos[1]);
+    boolean offsetPressed = pos[0] > 0 && pos[1] > 0;
 
-    return pos[0] > 0 && pos[1] > 0;
+    if(offsetPressed)
+        Serial.printf("%d,%d\n", pos[0], pos[1]);
+
+    return offsetPressed;
 }
 
-void startMeasure(void)
+void startMeasureOffsetButton(void)
 {   
   Serial.println();
-  Serial.println("Time measurement started");
+  Serial.println("OffsetButton Time measurement started");
   startTimeButtonPressed = millis();
 }
 
-void endMeasure(void)
+void endMeasureOffsetButton(void)
 {
   Serial.println();
-  Serial.println("Time measurement ended");
+  Serial.println("OffsetButton measurement ended");
   endTimeButtonPressed = millis();
 
   unsigned long result = endTimeButtonPressed - startTimeButtonPressed;
@@ -97,7 +98,11 @@ void endMeasure(void)
 // If button was pressed long enough => Set Offset
   if(result > BUTTON_TIME_SET_OFFSET) {
     // setOffsetsToEEPROM(getHorizontalAngleRaw(), getVerticalAngleRaw());
-    Serial.print(result);Serial.println("Set offset");
+    Serial.print(result);Serial.println("Set offset to:");
+
+    Serial.print("New H:");Serial.println(getHorizontalAngleRaw());
+    Serial.print("New V:");Serial.println(getVerticalAngleRaw());
+
     return;
   }
 
