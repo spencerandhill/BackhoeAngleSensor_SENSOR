b1e33dc8057460ecc6a5c102c3618924826886eb
spencerandhill <jan.rathmer@gmail.com>
2021-11-01 20:40:49 +0100

    VERSION 0.1 Range of X-Slider adjusted, comment typo fixed

--- BackhoeAngleSensor_lvgl.ino
+++ BackhoeAngleSensor_lvgl.ino
@@ -1,3 +1,7 @@
+// Version: 0.1
+// First working version: -Full hardware support for Display (including LVGL), Touch-Driver and BNO055 Sensor.
+//                        -No Sensor Offset calibration, yet.
+
 #include "makerfabs_pin.h"
 #include <Wire.h>
 #include <lvgl.h>
--- displaySensorPage.ino
+++ displaySensorPage.ino
@@ -8,11 +8,11 @@ static lv_obj_t * sliderY_label;
 void createSensorSliders()
 {
 // X-Slider    
-    /* Create a slider at the right of the display */
+    /* Create a slider at the bottom of the display */
     sliderX = lv_slider_create(lv_scr_act(), NULL);
     lv_obj_set_width(sliderX, LV_DPI * 2);
     lv_obj_align(sliderX, NULL, LV_ALIGN_CENTER, 0, 10);
-    lv_slider_set_range(sliderX, -128, 128);
+    lv_slider_set_range(sliderX, -20, 20);
     lv_obj_set_size(sliderX, 200, 50);
     lv_obj_set_pos(sliderX, 50, 200);
     lv_slider_set_value(sliderX, 0, LV_ANIM_OFF);
